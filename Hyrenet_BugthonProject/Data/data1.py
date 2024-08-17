ba7cdffc8bb240e2ff9a815cedf50968ae889976
Gayathri11-2023 <150446843+Gayathri11-2023@users.noreply.github.com>
2024-08-17 21:01:18 +0530

    file uploaded

---
+++ Hyrenet_BugthonProject/Data/data1.py
@@ -0,0 +1,13 @@
+class WebData:
+    url ="https://app.hyrenet.in/"
+    email = " hyrenet+bugathon@guvi.in"
+    password = "hyrenettest@123"
+    invalidpwd = "Invalidpassword"
+    templateName = "TestEngineer - Manual&Automation"
+    objDuration = "30"
+    testdescription =(" We are seeking a detail-oriented and experienced Test Engineer to join our quality assurance team. The Test Engineer will be responsible for designing, implementing, "
+                      "and executing test plans to ensure the quality and functionality of our software products. ")
+    testnameLocator = "Test engineer -manual and automation"
+
+
+
